448d37b31c5f2cb4b95e38426d95885706ed0915
g3zb0yy <b0d3ns33@criminal.li>
2021-02-15 17:51:32 +0100

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -11,15 +11,7 @@ def load_key():
 
 def decrypt(dir, key):
     try:
-        global decrypted_data
         f = Fernet(key)
-        for filename in glob.glob(os.path.join(dir, '*.*')):
-            with open(filename, "rb") as file:
-                encrypted_data = file.read()
-            decrypted_data = f.decrypt(encrypted_data)
-            with open(filename, "wb") as file:
-                file.write(decrypted_data)
-
         subdirs = [x[0] for x in os.walk(dir)]
         for dirdir in subdirs:
             for filename in glob.glob(os.path.join(dirdir, '*.*'), recursive=True):
