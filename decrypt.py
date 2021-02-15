28341a73917735a91173ccce1d1611532b6359e5
g3zb0yy <b0d3ns33@criminal.li>
2021-02-15 16:44:48 +0100

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -10,14 +10,26 @@ def load_key():
     return a.encode()
 
 def decrypt(dir, key):
-    global decrypted_data
-    f = Fernet(key)
-    for filename in glob.glob(os.path.join(dir, '*.txt')):
-        with open(filename, "rb") as file:
-            encrypted_data = file.read()
-        decrypted_data = f.decrypt(encrypted_data)
-        with open(filename, "wb") as file:
-            file.write(decrypted_data)
+    try:
+        global decrypted_data
+        f = Fernet(key)
+        for filename in glob.glob(os.path.join(dir, '*.*')):
+            with open(filename, "rb") as file:
+                encrypted_data = file.read()
+            decrypted_data = f.decrypt(encrypted_data)
+            with open(filename, "wb") as file:
+                file.write(decrypted_data)
+
+        subdirs = [os.path.join(dir, o) for o in os.listdir(dir) if os.path.isdir(os.path.join(dir, o))]
+        for dirdir in subdirs:
+            for filename in glob.glob(os.path.join(dirdir, '*.*'), recursive=True):
+                with open(filename, "rb") as file:
+                    encrypted_data = file.read()
+                decrypted_data = f.decrypt(encrypted_data)
+                with open(filename, "wb") as file:
+                    file.write(decrypted_data)
+    except:
+        pass
 
 key = load_key()
 dir = "fichiers"
